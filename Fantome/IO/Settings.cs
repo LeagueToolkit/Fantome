27d288a91cab971fd4318a847f8cfa27981e4e9b
Crauzer <filip.quitko@gmail.com>
2017-05-27 12:04:02 +0200

    ConfigFile => Settings

---
+++ Fantome/IO/Settings.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Fantome.IO
+{
+    public class Settings
+    {
+        public String LeagueOfLegendsPath { get; private set; }
+        public void Save(String location)
+        {
+            File.WriteAllText(location, JsonConvert.SerializeObject(this, Formatting.Indented));
+        }
+        public static Settings GetInstance(String location)
+        {
+            return JsonConvert.DeserializeObject<Settings>(File.ReadAllText(location));
+        }
+    }
+}
