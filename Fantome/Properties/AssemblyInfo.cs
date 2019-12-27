ded71c8c73cc2d70eb036bb59f8b5985878ea167
Crauzer <filip.quitko@gmail.com>
2019-12-27 21:02:15 +0100

    Fix windows checking

--- Fantome/Properties/AssemblyInfo.cs
+++ Fantome/Properties/AssemblyInfo.cs
@@ -40,7 +40,6 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -51,5 +50,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
