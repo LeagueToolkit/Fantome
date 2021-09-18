07b21f4dd9e08a989adf25cb97a1ab38f7ca3d4a
Crauzer <filip.quitko@gmail.com>
2021-09-18 22:39:02 +0200

    wipe

--- Fantome/Utilities/MaterialDesign/ModifierBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace Fantome.Utilities.MaterialDesign
-{
-    public abstract class ModifierBase
-    {
-        public abstract void Apply(DependencyObject target);
-    }
-}
