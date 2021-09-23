3ba43bd8d1c55bd5de23ec5970116c6f5f43c952
Crauzer <filip.quitko@gmail.com>
2021-09-23 23:55:10 +0200

    extractor base

---
+++ Fantome/Pages/Extractor.razor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fantome.Store.Modules.Extractor;
+using Fluxor;
+using Microsoft.AspNetCore.Components;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace Fantome.Pages
+{
+    public partial class Extractor
+    {
+        [Inject] public IDispatcher Dispatcher { get; set; }
+
+        public void OpenWadFile()
+        {
+            CommonOpenFileDialog commonOpenFileDialog = new();
+
+            commonOpenFileDialog.Filters.Add(new("WAD Files", "*.wad.client;*.wad"));
+
+            if(commonOpenFileDialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                this.Dispatcher.Dispatch(new AddWadAction() { WadFileLocation = commonOpenFileDialog.FileName });
+            }
+        }
+    }
+}
---
+++ Fantome/Store/Modules/Extractor/Actions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantome.Store.Modules.Extractor
+{
+    public class AddWadAction
+    {
+        public string WadFileLocation { get; set; }
+    }
+    public class AddWadActionSuccess
+    {
+
+    }
+    public class AddWadActionError
+    {
+        public Exception Error { get; set; }
+    }
+}
