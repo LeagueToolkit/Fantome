0052709bcf689c80865b88583ade5804a16922b9
Crauzer <filip.quitko@gmail.com>
2021-09-23 23:54:40 +0200

    add some action interfaces

---
+++ Fantome/Store/Actions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantome.Store
+{
+    public interface ConfigAction { }
+    
+    public interface AsyncAction { }
+    public interface AsyncActionRequest : AsyncAction { }
+    public interface AsyncActionSuccess : AsyncAction { }
+    public interface AsyncActionFailure : AsyncAction
+    {
+        Exception Error { get; set; }
+    }
+}
