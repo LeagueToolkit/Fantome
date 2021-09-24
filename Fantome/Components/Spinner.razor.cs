dfaca0bf85e22c0c72411def65af01698ce762a4
Crauzer <filip.quitko@gmail.com>
2021-09-24 08:56:17 +0200

    Add Message to spinner

--- Fantome/Components/Spinner.razor.cs
+++ Fantome/Components/Spinner.razor.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 using MudBlazor;
 
 namespace Fantome.Components
 {
     public partial class Spinner
     {
-
+        [Parameter] public string Message { get; set; }
     }
 }
